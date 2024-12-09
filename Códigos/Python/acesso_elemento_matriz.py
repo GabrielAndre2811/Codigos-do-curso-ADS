f378d94118b0cf9d54b7b40a40e8856e802468cf
GABRIEL ANDRE GONCALVES <bielandregoncalves@gmail.com>
2024-12-09 14:03:19 -0300

    Add files via upload

---
+++ Códigos/Python/acesso_elemento_matriz.py
@@ -0,0 +1,11 @@
+# Definindo uma matriz 3x3
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Acessando o elemento na terceira linha e segunda coluna
+elemento = matriz[2][1] ##linha [coluna]
+
+print("Elemento na terceira linha e segunda coluna:", elemento)
