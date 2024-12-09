237c952a2bc8ec096aa59e86604f223fb11251a8
GABRIEL ANDRE GONCALVES <bielandregoncalves@gmail.com>
2024-12-09 14:10:21 -0300

    Add files via upload

---
+++ Códigos/Python/append_listadentrodalista.py
@@ -0,0 +1,9 @@
+# Listas iniciais
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+a.append(b)
+print(a)  # Saída: [1, 2, 3, [4, 5, 6]]
+
+
+## A opção a.append(b)  adicionaria a lista b como um único elemento na lista a, criando uma lista dentro de outra.
